f653869b04f3bf46771d9eb6fa9af52ad530b1ef
hjc2000 <hjc.666@foxmail.com>
2023-06-15 10:50:51 +0800

    将文件选取全部改成使用接口

--- StringLib/Xml.cs
+++ StringLib/Xml.cs
@@ -6,8 +6,8 @@ public static class Xml
 {
 	public static string ObjToXml<T>(object? o)
 	{
-		XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
-		MemoryStream memoryStream = new MemoryStream();
+		XmlSerializer xmlSerializer = new(typeof(T));
+		MemoryStream memoryStream = new();
 		using (memoryStream)
 		{
 			xmlSerializer.Serialize(memoryStream, o);
@@ -17,16 +17,16 @@ public static class Xml
 
 	public static void ObjToXmlStream<T>(object? o, Stream stream)
 	{
-		XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+		XmlSerializer xmlSerializer = new(typeof(T));
 		xmlSerializer.Serialize(stream, o);
 		stream.Position = 0;
 	}
 
 	public static T? XmlToObj<T>(string xml)
 	{
-		XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+		XmlSerializer xmlSerializer = new(typeof(T));
 		byte[] buff = Encoding.UTF8.GetBytes(xml);
-		MemoryStream memoryStream = new MemoryStream(buff);
+		MemoryStream memoryStream = new(buff);
 		using (memoryStream)
 		{
 			return (T?)xmlSerializer.Deserialize(memoryStream);
@@ -35,7 +35,7 @@ public static class Xml
 
 	public static T? XmlStreamToObj<T>(Stream xmlStream)
 	{
-		XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+		XmlSerializer xmlSerializer = new(typeof(T));
 		return (T?)xmlSerializer.Deserialize(xmlStream);
 	}
 }
