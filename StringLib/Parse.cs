e18e31dc178b6558e959308dee46ae90b486b768
hjc2000 <hjc.666@foxmail.com>
2023-06-27 16:49:28 +0800

    下载 oscam.txt

--- StringLib/Parse.cs
+++ StringLib/Parse.cs
@@ -2,6 +2,12 @@
 
 public static class Parse
 {
+	/// <summary>
+	/// 解析 key = value 形式的字符串。key, value 支持使用单引号、双引号包起来，就像：
+	/// key = "value" , key = 'value' , 'key' = 'value' ...... 等
+	/// </summary>
+	/// <param name="str"></param>
+	/// <returns></returns>
 	public static (bool success, string key, string value) ParseKeyValue(this string str)
 	{
 		try
@@ -10,21 +16,46 @@ public static class Parse
 			int index = str.IndexOf('=');
 			string key = str[..index];
 			string value = str[(index + 1)..];
-			if (value.StartsWith('\"') || value.StartsWith('\''))
-			{
-				value = value[1..];
-			}
-
-			if (value.EndsWith('\"') || value.EndsWith('\''))
-			{
-				value = value[..^1];
-			}
-
+			key = RemoveQuotationMark(key);
+			value = RemoveQuotationMark(value);
 			return (true, key, value);
 		}
 		catch
 		{
 			return (false, string.Empty, string.Empty);
 		}
+	}
+
+	/// <summary>
+	/// 一直读取行（一次读取一行）知道读到一行，这一行中包含 match
+	/// </summary>
+	/// <param name="reader"></param>
+	/// <param name="match"></param>
+	public static void ReadLineUntil(this StringReader reader, string match)
+	{
+		while (true)
+		{
+			string? line = reader.ReadLine();
+			if (line == null) break;
+			if (line.Contains(match)) break;
+		}
+	}
+	/// <summary>
+	/// 去除字符串开头和结尾位置的引号
+	/// </summary>
+	/// <returns></returns>
+	private static string RemoveQuotationMark(string value)
+	{
+		if (value.StartsWith('\"') || value.StartsWith('\''))
+		{
+			value = value[1..];
+		}
+
+		if (value.EndsWith('\"') || value.EndsWith('\''))
+		{
+			value = value[..^1];
+		}
+
+		return value;
 	}
 }
