5b05088a9683cfd8f36f25cd3492dd4ebfdc8ee6
hjc2000 <huangjincanqqmail@qq.com>
2023-06-28 22:25:49 +0800

    完善 SafeNumberParser

--- StringLib/Parse/SafeNumberParser.cs
+++ StringLib/Parse/SafeNumberParser.cs
@@ -1,13 +1,64 @@
 ﻿using System.Globalization;
+using System.Numerics;
 
 namespace StringLib.Parse;
 
 /// <summary>
-/// 不会抛出异常的 parser，分析字符串，返回数值类型。分析失败会返回 0
+/// 不会抛出异常的 parser，分析字符串，返回数值类型。分析失败会返回 0。
+/// 能够自动识别 16 进制数和 10 进制数的字符串。（要求 16 进制数的字符串以 0x 前导符开始，10 进制数
+/// 没有任何修饰符）
 /// </summary>
 public static class SafeNumberParser
 {
-	public static int SafeParseInt32(string str)
+	public static sbyte ParseInt8(string str)
+	{
+		try
+		{
+			return str.StartsWith("0x") ? sbyte.Parse(str, NumberStyles.HexNumber) : sbyte.Parse(str);
+		}
+		catch
+		{
+			return 0;
+		}
+	}
+
+	public static byte ParseUint8(string str)
+	{
+		try
+		{
+			return str.StartsWith("0x") ? byte.Parse(str, NumberStyles.HexNumber) : byte.Parse(str);
+		}
+		catch
+		{
+			return 0;
+		}
+	}
+
+	public static short ParseInt16(string str)
+	{
+		try
+		{
+			return str.StartsWith("0x") ? short.Parse(str, NumberStyles.HexNumber) : short.Parse(str);
+		}
+		catch
+		{
+			return 0;
+		}
+	}
+
+	public static ushort ParseUint16(string str)
+	{
+		try
+		{
+			return str.StartsWith("0x") ? ushort.Parse(str, NumberStyles.HexNumber) : ushort.Parse(str);
+		}
+		catch
+		{
+			return 0;
+		}
+	}
+
+	public static int ParseInt32(string str)
 	{
 		try
 		{
@@ -19,7 +70,7 @@ public static class SafeNumberParser
 		}
 	}
 
-	public static uint SafeParseUInt32(string str)
+	public static uint ParseUInt32(string str)
 	{
 		try
 		{
@@ -31,7 +82,7 @@ public static class SafeNumberParser
 		}
 	}
 
-	public static ulong SafeParseUInt64(string str)
+	public static ulong ParseUInt64(string str)
 	{
 		try
 		{
@@ -43,11 +94,35 @@ public static class SafeNumberParser
 		}
 	}
 
-	public static long SafeParseInt64(string str)
+	public static long ParseInt64(string str)
 	{
 		try
 		{
 			return str.StartsWith("0x") ? long.Parse(str, NumberStyles.HexNumber) : long.Parse(str);
+		}
+		catch
+		{
+			return 0;
+		}
+	}
+
+	public static double ParseDouble(string str)
+	{
+		try
+		{
+			return str.StartsWith("0x") ? double.Parse(str, NumberStyles.HexNumber) : double.Parse(str);
+		}
+		catch
+		{
+			return 0;
+		}
+	}
+
+	public static BigInteger ParseBigInt(string str)
+	{
+		try
+		{
+			return str.StartsWith("0x") ? BigInteger.Parse(str, NumberStyles.HexNumber) : BigInteger.Parse(str);
 		}
 		catch
 		{
