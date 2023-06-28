dd0d3fb978a16ea7f61f4d536a8a40994560f3c8
hjc2000 <hjc.666@foxmail.com>
2023-06-28 16:59:55 +0800

    1

---
+++ StringLib/Parse/KeyValueParser.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+
+namespace StringLib.Parse;
+
+public static class KeyValueParser
+{
+    /// <summary>
+    /// 解析 key = value 形式的字符串。key, value 支持使用单引号、双引号包起来，就像：
+    /// key = "value" , key = 'value' , 'key' = 'value' ...... 等
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    public static (bool success, string key, string value) ParseKeyValue(this string str)
+    {
+        try
+        {
+            str = str.Trim();
+            int index = str.IndexOf('=');
+            string key = str[..index];
+            string value = str[(index + 1)..];
+            key = Trim(key);
+            value = Trim(value);
+            return (true, key, value);
+        }
+        catch
+        {
+            return (false, string.Empty, string.Empty);
+        }
+    }
+
+    /// <summary>
+    /// 一直读取行（一次读取一行）直到读到一行，这一行内容等于 match
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="match"></param>
+    /// <exception cref="Exception">如果没读到 match 字符串就结束了会抛出异常</exception>
+    public static string ReadLineUntil(this StringReader reader, string match)
+    {
+        StringBuilder sb = new();
+        while (true)
+        {
+            string? line = reader.ReadLine();
+            if (line == null)
+            {
+                throw new Exception("没读到 match 字符串就结束了");
+            }
+            else
+            {
+                sb.AppendLine(line);
+                if (line == match) break;
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// 去除字符串开头和结尾位置的引号和空白符
+    /// </summary>
+    /// <returns></returns>
+    private static string Trim(string value)
+    {
+        if (value.StartsWith('\"') || value.StartsWith('\''))
+        {
+            value = value[1..];
+        }
+
+        if (value.EndsWith('\"') || value.EndsWith('\''))
+        {
+            value = value[..^1];
+        }
+
+        value = value.Trim();
+        return value;
+    }
+}
---
+++ StringLib/Parse/SafeNumberParser.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+
+namespace StringLib.Parse;
+
+/// <summary>
+/// 不会抛出异常的 parser，分析字符串，返回数值类型。分析失败会返回 0
+/// </summary>
+public static class SafeNumberParser
+{
+	public static int SafeParseInt32(string str)
+	{
+		try
+		{
+			return str.StartsWith("0x") ? int.Parse(str, NumberStyles.HexNumber) : int.Parse(str);
+		}
+		catch
+		{
+			return 0;
+		}
+	}
+
+	public static uint SafeParseUInt32(string str)
+	{
+		try
+		{
+			return str.StartsWith("0x") ? uint.Parse(str, NumberStyles.HexNumber) : uint.Parse(str);
+		}
+		catch
+		{
+			return 0;
+		}
+	}
+
+	public static ulong SafeParseUInt64(string str)
+	{
+		try
+		{
+			return str.StartsWith("0x") ? ulong.Parse(str, NumberStyles.HexNumber) : ulong.Parse(str);
+		}
+		catch
+		{
+			return 0;
+		}
+	}
+
+	public static long SafeParseInt64(string str)
+	{
+		try
+		{
+			return str.StartsWith("0x") ? long.Parse(str, NumberStyles.HexNumber) : long.Parse(str);
+		}
+		catch
+		{
+			return 0;
+		}
+	}
+}
