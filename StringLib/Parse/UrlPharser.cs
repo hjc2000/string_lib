35cf29512bca5f6d7b7944029bbbeecb84fb686b
hjc2000 <huangjincanqqmail@qq.com>
2023-07-07 18:35:07 +0800

    OnConfigfileChanged 中首先执行   StateHasChanged(); 避免无网络状态下加载配置文件会因为卡在 AutoTest 导致界面始终没更新

---
+++ StringLib/Parse/UrlPharser.cs
@@ -0,0 +1,23 @@
+﻿namespace StringLib.Parse;
+public static class UrlPharser
+{
+	/// <summary>
+	/// * 将类似 http://localhost:9000/web/ 的 url 取出其中的基础部分
+	/// http://localhost:9000/ 进行返回
+	/// * 解析失败会返回空字符串
+	/// </summary>
+	/// <returns></returns>
+	public static string GetBaseUrl(this string url)
+	{
+		try
+		{
+			int index1 = url.IndexOf("//");
+			int index2 = url.IndexOf("/", index1 + 2);
+			return url[..(index2 + 1)];
+		}
+		catch
+		{
+			return string.Empty;
+		}
+	}
+}
